0180e0f14f8e9fb74ce23f74524ad66a014bbae4
aw1875 <aw1875@rit.edu>
2023-06-19 16:22:31 -0500

    feat: pushing code

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    endOfLine: 'lf',
+    semi: true,
+    tabWidth: 2,
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: 'none'
+}
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { LRUCache } from 'lru-cache';
+import { NextRequest } from 'next/server';
+
+type Options = {
+  uniqueTokenPerInterval?: number;
+  interval?: number;
+};
+
+export default function rateLimit(options?: Options) {
+  const tokenCache = new LRUCache<string, number[]>({
+    max: options?.uniqueTokenPerInterval || 500,
+    ttl: options?.interval || 60000
+  });
+
+  return {
+    checkNext: (req: NextRequest, limit: number) => {
+      const headers: Headers = new Headers();
+      const token =
+        req.ip ||
+        req.headers.get('x-forwarded-for') ||
+        req.headers.get('x-real-ip') ||
+        req.headers.get('x-client-ip') ||
+        '127.0.0.1';
+      const tokenCount = (tokenCache.get(token) as number[]) || [0];
+
+      if (tokenCount[0] === 0) tokenCache.set(token, tokenCount);
+
+      tokenCount[0] += 1;
+
+      const currentUsage = tokenCount[0];
+      const isRateLimited = currentUsage >= limit;
+      headers.set('X-RateLimit-Limit', limit.toString());
+      headers.set('X-RateLimit-Remaining', isRateLimited ? '0' : (limit - currentUsage).toString());
+
+      if (isRateLimited) throw new Error('Rate limit exceeded');
+
+      return headers;
+    }
+  };
+}
